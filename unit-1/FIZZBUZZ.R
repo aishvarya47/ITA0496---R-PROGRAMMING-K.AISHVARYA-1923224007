7ce8138122f7ec183e67a66484af0164dc810b97
aishvarya47 <154430479+aishvarya47@users.noreply.github.com>
2025-02-06 09:18:11 +0530

    FIZZBUZZ FROM 1 TO 100

---
+++ unit-1/FIZZBUZZ.R
@@ -0,0 +1,13 @@
+cat("\nFizzBuzz:\n")
+for (i in 1:100) {
+  if (i %% 3 == 0 & i %% 5 == 0) {
+    cat("FizzBuzz ")
+  } else if (i %% 3 == 0) {
+    cat("Fizz ")
+  } else if (i %% 5 == 0) {
+    cat("Buzz ")
+  } else {
+    cat(i, " ")
+  }
+}
+cat("\n")
