7c4ae4bb186c3724fd33b0a14efbde98caebbf03
aishvarya47 <154430479+aishvarya47@users.noreply.github.com>
2025-02-06 09:19:53 +0530

    EXTRACT FIRST 10 LOWER AND LAST 10 UPPER CASE

---
+++ unit-1/LOWERCASE EXTRACTION.R
@@ -0,0 +1,6 @@
+first_10_lower <- letters[1:10]
+last_10_upper <- LETTERS[17:26]
+range_22_24_upper <- LETTERS[22:24]
+cat("\nFirst 10 Lowercase Letters:\n", first_10_lower, "\n")
+cat("Last 10 Uppercase Letters:\n", last_10_upper, "\n")
+cat("Letters from 22nd to 24th (Uppercase):\n", range_22_24_upper, "\n")
